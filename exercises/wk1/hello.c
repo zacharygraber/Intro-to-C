12ae8ac5a9a499d81c27b4d9c72ebbbeb0cca7a8
Zachary E Graber <zegraber@silo.soic.indiana.edu>
2020-08-28 15:26:12 -0400

    First commit. I believe Assignment1 is done HERE

--- exercises/wk1/hello.c
+++ exercises/wk1/hello.c
@@ -1,16 +1,22 @@
 /* 
- * Basic C program to print out hello world! 
- * Author: Adeel Bhutta
+ * Basic C Program to print out a greeting, then count down from 100.
+ * Author: Zachary E. Graber (zegraber@iu.edu)
+ *
+ * Modified from a program intended to print out hello world by Adeel Bhutta
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
   int myNumber = 0;
-  printf("Hello: Welcome to C291!\n");
-  printf("Please enter a number and press ENTER: ");
-  scanf("%d", &myNumber);  
-  printf("Here is the number yor entered: %d\n",myNumber);
+  printf("\" *****\"{Welcome to Fall2020 - 'Corona-affected' 8-Week session @ IU}, \\\\ Every week, please start early and ask for help! Also, let's try to stay safe! //\"***** \"\n");
+  int countdown = 100;
+  while (1) { // 1 simply expesses a true condition, making this an infinite loop.
+    printf("%.3d .....\n", countdown);
+    countdown = countdown - 1;
+    sleep(1);
+  }
   return(0); 
 }
 
