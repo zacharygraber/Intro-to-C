c7cf3d8818b15295f42df8e5d90ea103a25a84b3
Zachary E. Graber <zegraber@iu.edu>
2020-09-04 21:36:18 -0400

    Made progress on file3, but it still has one bug or so to go.

--- exercises/wk2/file3.c
+++ exercises/wk2/file3.c
@@ -13,41 +13,51 @@
 int main(void){
   char input;
   int num1, num2, result, count;
-   
-  while(input == W )
-  {
-  printf("Welcome to the Calculator\nOperation choices:\tAddition(A)\n\t\t\tSubtraction(S)\n\t\t\tMultiplication(M)\n\t\t\tDivision(D)\nEnter choice: ");
-
-  scanf("%f", input);
-
-  if(input = A || input == 'S' && input == 'M' || input == 'D'){
-    printf("Enter both numbers in required sequence: ");
-    scanf("%d, %d", num1 &num2);
-    switch(input){
-        case 'A': 
-                result += num1 << num2;
-                break;  
-        case 'R': 
-                result -= num || num2;
-        case 'M': 
-                result *= num1 ** num2;  
-        default:
-                result == num2 % num1;
-    }
-    if(result < -500){
-      printf("Greater than 50.\n");
-    }
-    else if(res == 50) {
-      printf("Less than 50\n");
-    }
-    else if{
-      printf("Equal to 50\n");
-    }
-    count += count;
-  }else{
-    printf("Please choose a valid operation\n");
   
-  scanf("Number of operations performed: %d",count);
-  printf("Quit the menu.\n");
+  while (1) {
+      printf("Welcome to the Calculator\nOperation choices:\tAddition(A)\n\t\t\tSubtraction(S)\n\t\t\tMultiplication(M)\n\t\t\tDivision(D)\n\t\t\tQuit(Q)\nEnter choice: ");
+
+      scanf("%c", &input);
+
+      if (input == 'A' || input == 'S' || input == 'M' || input == 'D') { // The case where valid input is given
+          printf("Enter both numbers in required sequence: ");
+          scanf("%d, %d", &num1, &num2);
+          
+          float result;
+          switch (input) {
+          case 'A': 
+              result = num1 + num2;
+              break;  
+          case 'S': 
+              result = num1 - num2;
+              break;
+          case 'M': 
+              result = num1 * num2;
+              break;
+          case 'D':
+              result = (float) num1 / num2;
+              break;
+          }
+
+          if (result > 100) {
+              printf("Greater than 100.\n");
+          }
+          else if (result == 100) {
+              printf("Equal to 100.\n");
+          }
+          else if (result < 100) {
+              printf("Less than 100.\n");
+          }
+          // Then increment the counter by 1, since 1 operation was performed.
+          count += 1;
+      }
+      else if (input == 'Q' || input == 'q') { // Acts as our sentinel value.
+          break;
+      }
+      else { // The case where valid input is not given; goes back to the top of the while() loop
+          printf("Please choose a valid operation\n");
+      }
+  }
+  printf("Number of operations performed: %d\n",count);
   return(0);
 }
