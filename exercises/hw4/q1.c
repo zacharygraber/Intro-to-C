41a69236b58904610acde32c3539b7062c52e63b
Zachary E. Graber <zegraber@iu.edu>
2020-09-23 16:35:41 -0400

    created skeleton for problem 1

---
+++ exercises/hw4/q1.c
@@ -0,0 +1,12 @@
+/*******************************************************
+ * This program solves CSCI-C 291 Hw4 problem 1	       *
+ * Author: Zachary E Graber (zegraber@iu.edu)	       *
+ * Date: 9/23/2020				       *
+ *******************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return(0);
+}
