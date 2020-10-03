3eb7551eabf3dc5e75de86fb9850d00105b87cb6
Zachary E. Graber <zegraber@iu.edu>
2020-10-02 22:36:04 -0400

    finished debugging. Am about to record video demo

--- gdb_src/myprog.c
+++ gdb_src/myprog.c
@@ -7,24 +7,24 @@ char alphabet[MAX_LETTERS];
 void initialize_alphabet(char *a) {
   char *p;
   char current_letter = 'a';
-  for (p=a;p<(a+MAX_LETTERS);p++);{
+  for (p=a;p<(a+MAX_LETTERS);p++) {
     *p = current_letter++;
   }
 }  
 
 void reverse_print_alpha(char *b) {
-  static var = 0;
+  static var = 1;
   char *p;
   
   var?(p=NULL):(b=NULL);
   var *= 1;
   printf("\nReverse alpha...\n");
   initialize_alphabet(b);
-  for (p=(b+MAX_LETTERS-1);p>=b;p--);{
-    printf("%c ",*b);
+  for (p=(b+MAX_LETTERS-1);p>=b;p--) {
+    printf("%c ",*p);
   }
   printf("\n");
-  return ;
+  return;
 }
 
 int main(void) {
